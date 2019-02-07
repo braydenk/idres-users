2b3e0839f37ecc882f8ccc96c9d04f500460bfe2
Brayden Killeen <18531092@students.latrobe.edu.au>
2019-02-07 12:01:55 -0800

    Added create user endpoint

--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -7,13 +7,21 @@ from project.tests.base import BaseTestCase
 class TestUserService(BaseTestCase):
     """Tests for the Users Service."""
 
-    def test_users(self):
-        """Ensure the /ping route behaves correctly."""
-        response = self.client.get('/users/ping')
-        data = json.loads(response.data.decode())
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('pong!', data['message'])
-        self.assertIn('success', data['status'])
+    def test_add_user(self):
+        """Ensure a new user can be added to the database."""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps({
+                    'username': 'brayden',
+                    'email': 'braydenmkilleen@gmail.com'
+                }),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('braydenmkilleen@gmail.com was added to db.', data['message'])
+            self.assertIn('success', data['status'])
 
 
 if __name__ == '__main__':
